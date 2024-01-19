d94973551843bd790058d4beef680b06c22a89ab
harpreet-singh-147 <h_singh_007@hotmail.co.uk>
2024-01-19 18:28:26 +0000

    takeaway task from week 1 - 'Code up your portfolio homepage'

---
+++ script/app.js
@@ -0,0 +1,61 @@
+const mainEl = document.querySelector('.main');
+const scrolledMainEl = document.querySelector('.scrolled');
+const mainHeroContainer = document.querySelector('.main__hero-container');
+const mainContentContainer = document.querySelector('.main__content-container');
+const hero = document.querySelector('.hero');
+const navLinks = document.querySelectorAll('.header__nav-list a');
+const logo = document.querySelector('.header__nav-logo');
+
+navLinks.forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    const targetSection = document.getElementById(link.className);
+    if (link.className === 'home-link') {
+      mainContentContainer.style.opacity = '';
+      window.scrollTo(0, 0);
+      history.pushState(null, '', 'index.html');
+    } else {
+      mainEl.classList.add('scrolled');
+      setTimeout(() => {
+        targetSection.scrollIntoView();
+      }, 0);
+      history.pushState(null, '', `#${link.className}`);
+    }
+  });
+});
+
+logo.addEventListener('click', e => {
+  e.preventDefault();
+  mainContentContainer.style.opacity = '';
+  window.scrollTo(0, 0);
+  history.pushState(null, '', 'index.html');
+});
+
+document.addEventListener('scroll', () => {
+  if (window.scrollY > 0) {
+    mainEl.classList.add('scrolled');
+    mainContentContainer.style.opacity = 1;
+    mainHeroContainer.style.opacity = 1;
+  } else {
+    mainContentContainer.style.opacity = '';
+    mainHeroContainer.style.opacity = '';
+    setTimeout(() => {
+      mainEl.classList.remove('scrolled');
+    }, 150);
+  }
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const hash = window.location.hash.slice(1);
+  const targetSection = hash ? document.getElementById(hash) : null;
+
+  if (targetSection) {
+    mainEl.classList.add('scrolled');
+    setTimeout(() => {
+      targetSection.scrollIntoView();
+    }, 500);
+  } else {
+    mainEl.classList.remove('scrolled');
+    window.scrollTo(0, 0);
+  }
+});
