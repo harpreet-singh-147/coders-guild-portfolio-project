b92e52646ce5fe305aa4dfe394e734ba1efb0925
harpreet-singh-147 <h_singh_007@hotmail.co.uk>
2024-01-24 02:02:03 +0000

    added function to monitor scroll &  width to remove classes

--- script/utils.js
+++ script/utils.js
@@ -3,6 +3,8 @@ import {
   navLinks,
   homeLink,
   spaceInUpAnimationClassNames,
+  headerEl,
+  navLogoSpan,
 } from './selectors.js';
 
 export const updateMainContent = (opacity, scrollToTop) => {
@@ -17,7 +19,7 @@ export const removeAndAddNavClass = () => {
   navLinks.forEach(link => {
     link.classList.remove('active-link');
   });
-
+  history.pushState(null, '', '/');
   homeLink.classList.add('active-link');
 };
 
@@ -27,4 +29,14 @@ export const removeAnimationsOnLoad = () => {
       animation.classList.remove('spaceInUp');
     });
   }, 500);
+};
+
+export const updateClassesBasedOnScrollAndWidth = () => {
+  const hasScrolled = window.scrollY > 0;
+  const isNarrowScreen = window.matchMedia('(max-width: 1000px)').matches;
+
+  if (hasScrolled && isNarrowScreen) {
+    headerEl.classList.remove('header__scrolled-bg');
+    navLogoSpan.classList.remove('scrolled-color');
+  }
 };
